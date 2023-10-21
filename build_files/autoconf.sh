19239314ed12850fd610eb56a154b7540f0505db
Reha Yağcıoğlu <78363381+ozreha@users.noreply.github.com>
2023-10-21 01:45:48 -0700

    Add files via upload

---
+++ build_files/autoconf.sh
@@ -0,0 +1,17 @@
+./blender/build_files/build_environment/install_deps_rpi.sh \
+--with-all \
+ --build-python \
+ --build-numpy \
+ --build-boost \
+ --build-ocio \
+ --build-openexr \
+ --build-oiio \
+--skip-llvm \
+--skip-osl \
+--skip-osd \
+--skip-openvdb \
+ --build-alembic \
+ --build-opencollada \
+--skip-embree \
+ --build-ffmpeg \
+
