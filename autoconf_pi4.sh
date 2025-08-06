91954dab48b300d3e2398aaf0987c1173d7f4671
Reha Yağcıoğlu <78363381+ozreha@users.noreply.github.com>
2025-08-06 20:03:59 +0300

    Update autoconf_pi4.sh

--- autoconf_pi4.sh
+++ autoconf_pi4.sh
@@ -1,4 +1,4 @@
-./blender-rpi-v2.79/build_files/build_environment/install_deps_pi4.sh \
+./blender279x/build_files/build_environment/install_deps_pi4.sh \
 --with-all \
 --threads=3 \
  --build-python \
